c7eec35e6f87cc1599c869ea645fa32bbcd87ea0
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2018-08-31 16:56:15 -0400

    Add ARM container, along with Python and ZWave libraries

---
+++ iot-devices/thing.py
@@ -0,0 +1,25 @@
+import requests
+from  openzwave.network import ZWaveNetwork
+from openzwave.option import ZWaveOption
+
+from flask import Flask, Response, jsonify
+from flask import request as flask_request
+
+from ddtrace import tracer, patch, config
+from ddtrace.contrib.flask import TraceMiddleware
+
+
+# Tracer configuration
+tracer.configure(hostname='agent')
+patch(requests=True)
+
+# enable distributed tracing for requests
+# to send headers (globally)
+config.requests['distributed_tracing'] = True
+
+app = Flask('thing')
+traced_app = TraceMiddleware(app, tracer, service='edge-thing')
+
+@app.route('/')
+def hello():
+    return Response({'hello': 'world'}, mimetype='application/json')
