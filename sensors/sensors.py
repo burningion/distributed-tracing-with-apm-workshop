67e6fbc98536ed4bcd8e79d8ce17a7c63f1b9ca4
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2018-09-04 15:53:28 -0400

    Start sensor API

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -1,6 +1,4 @@
 import requests
-from  openzwave.network import ZWaveNetwork
-from openzwave.option import ZWaveOption
 
 from flask import Flask, Response, jsonify
 from flask import request as flask_request
@@ -8,6 +6,9 @@ from flask import request as flask_request
 from ddtrace import tracer, patch, config
 from ddtrace.contrib.flask import TraceMiddleware
 
+import random
+
+sensors = []
 
 # Tracer configuration
 tracer.configure(hostname='agent')
@@ -23,3 +24,14 @@ traced_app = TraceMiddleware(app, tracer, service='edge-thing')
 @app.route('/')
 def hello():
     return Response({'Hello from Sensors': 'world'}, mimetype='application/json')
+
+# forget REST for now
+@app.route('/add_sensor')
+def add_sensor():
+    sensors.append({'sensor_no': len(sensors) + 1, 'value': random.randint(1,100)})
+    return jsonify(sensors)
+
+@app.route('/get_sensors')
+def get_sensors():
+    return jsonify({'sensor_count': len(sensors),
+                    'system_status': sensors})
