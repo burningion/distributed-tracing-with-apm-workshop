cc811afbd48ce2b6e4fcfab756ebf8b7bb8588c3
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2018-09-05 16:27:35 -0400

    Add and enable the JS trace agent

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,9 @@
+const tracer = require('dd-trace').init({hostname:'agent'})
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    res.send({'hello': 'world'})
+})
 
 app.listen(5003, () => console.log('Example app listening on port 5003!'))
