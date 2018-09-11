23aa3af68a2dd6243f8b225be85b2122a8397911
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2018-09-11 12:48:22 -0400

    Getting ready to hook redis up to node

--- node-api/server.js
+++ node-api/server.js
@@ -2,20 +2,30 @@ const tracer = require('dd-trace').init({hostname:'agent'})
 const express = require('express')
 const app = express()
 
+users = {'users': [{'name': 'City of Dorondo', 'id': 1, 'demand_gph': 100},
+        {'name': 'Cortland County', 'id': 2, 'demand_gph': 200},
+        {'name': 'C3 Energy', 'id': 3, 'demand_gph': 4000}]}
+
 app.get('/', (req, res) => {
-    res.send({'hello': 'world'})
+    res.send({'Hello from Water Usage': 'World!'})
 })
 
 app.get('/users', async (req, res) => {
-    await res.send({'users': [{'name': 'City of Dorondo', 'id': 1, 'demand_gph': 100},
-                    {'name': 'Cortland County', 'id': 2, 'demand_gph': 200},
-                    {'name': 'C3 Energy', 'id': 3, 'demand_gph': 4000}]})
+    await res.send(users)
 })
 
 app.get('/users-alt', (req, res) => {
-    res.send({'users': [{'name': 'City of Dorondo', 'id': 1, 'demand_gph': 100},
-                        {'name': 'Cortland County', 'id': 2, 'demand_gph': 200},
-                        {'name': 'C3 Energy', 'id': 3, 'demand_gph': 4000}]})
+    res.send(users)
+})
+
+app.post('/users/:userId(\\d+)/:waterLevel(\\d+)', async (req, res) => {
+    try {
+        const waterLevel = parseInt(req.params.waterLevel)
+        const userId = parseInt(req.params.userId)
+        await res.send({'name': 'City of Dorado', 'id': userId, 'demand_gph': waterLevel})    
+    } catch (e) {
+        res.status(500).end()
+    }
 })
 
 app.listen(5003, () => console.log('User demand API listening on port 5003!'))
