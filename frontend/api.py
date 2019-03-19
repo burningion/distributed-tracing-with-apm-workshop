c29a2dd0b6107a0da3b98cfc7997511b7b592ba1
Kirk Kaiser <kirk.kaiser@datadoghq.com>
2019-03-19 10:23:08 -0400

    Convert to f-strings with kubernetes env vars for pods

--- frontend/api.py
+++ frontend/api.py
@@ -12,10 +12,16 @@ from ddtrace.ext.priority import USER_REJECT, USER_KEEP
 import subprocess
 import random
 
+from os import environ
+
 app = Flask('api')
 
 if os.environ['FLASK_DEBUG']:
     CORS(app)
+
+SENSORS_URL = f"http://{environ['SENSORS_API_SERVICE_HOST']}:{environ['SENSORS_API_SERVICE_PORT_HTTP']}"
+PUMPS_URL = f"http://{environ['PUMPS_SERVICE_SERVICE_HOST']}:{environ['PUMPS_SERVICE_SERVICE_PORT']}"
+NODE_URL = f"http://{environ['NODE_API_SERVICE_HOST']}:{environ['NODE_API_SERVICE_PORT_HTTP']}"
 
 @app.route('/')
 def homepage():
@@ -27,30 +33,30 @@ def service_worker_js():
 
 @app.route('/status')
 def system_status():
-    status = requests.get('http://sensors:5002/sensors').json()
+    status = requests.get(f"{SENSORS_URL}/sensors").json()
     app.logger.info(f"Sensor status: {status}")
-    pumps = requests.get('http://pumps:5001/devices').json()
-    users = requests.get('http://noder:5004/users').json()
+    pumps = requests.get(f"{PUMPS_URL}/devices").json()
+    users = requests.get(f"{NODE_URL}/users").json()
     return jsonify({'sensor_status': status, 'pump_status': pumps, 'users': users})
 
 @app.route('/users', methods=['GET', 'POST'])
 def users():
     if flask_request.method == 'POST':
         newUser = flask_request.get_json()
-        userStatus = requests.post('http://noder:5004/users', json=newUser).json()
+        userStatus = requests.post(f"{NODE_URL}/users", json=newUser).json()
         return jsonify(userStatus)
     elif flask_request.method == 'GET':
-        users = requests.get('http://noder:5004/users').json()
+        users = requests.get(f"{NODE_URL}/users").json()
         return jsonify(users)
 
 @app.route('/add_sensor')
 def add_sensor():
-    sensors = requests.post('http://sensors:5002/sensors').json()
+    sensors = requests.post(f"{SENSORS_URL}/sensors").json()
     return jsonify(sensors)
     
 @app.route('/add_pump', methods=['POST'])
 def add_pump():
-    pumps = requests.post('http://pumps:5001/devices').json()
+    pumps = requests.post(f"{PUMPS_URL}/devices").json()
     app.logger.info(f"Getting {pumps}")
     return jsonify(pumps)
 
@@ -78,7 +84,7 @@ def call_generate_requests():
 # enable user sampling because low request count
 @app.route('/generate_requests_user')
 def call_generate_requests_user():
-    users = requests.get('http://noder:5004/users').json()
+    users = requests.get(f"{NODE_URL}/users").json()
     user = random.choice(users)
     span = tracer.current_span()
     span.context.sampling_priority = USER_KEEP
@@ -87,11 +93,11 @@ def call_generate_requests_user():
     output = subprocess.check_output(['/app/traffic_generator.py',
                                      '20',
                                      '100',
-                                     'http://noder:5004/users/' + user['uid']])
+                                     f"{NODE_URL}/users/" + user['uid']])
     app.logger.info(f"Chose random user {user['name']} for requests: {output}")
     return jsonify({'random_user': user['name']})
 
 @app.route('/simulate_sensors')
 def simulate_sensors():
-    sensors = requests.get('http://sensors:5002/refresh_sensors').json()
+    sensors = requests.get(f"{SENSORS_URL}/refresh_sensors").json()
     return jsonify(sensors)
