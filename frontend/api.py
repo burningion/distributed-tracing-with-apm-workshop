c65d64f7ec0ba962e632b70528b71c6d0f226330
Kirk Kaiser <kirk@zothcorp.com>
2019-04-05 13:14:40 -0400

    Eliminate redundant current span call

--- frontend/api.py
+++ frontend/api.py
@@ -61,8 +61,6 @@ def call_generate_requests():
     span = tracer.current_span()
     app.logger.info(f"Looking at {span}")
     app.logger.info(f"with span id {span.span_id}")
-
-    span = tracer.current_span()
     
     span.set_tags({'requests': payload['total'], 'concurrent': payload['concurrent']})
 
