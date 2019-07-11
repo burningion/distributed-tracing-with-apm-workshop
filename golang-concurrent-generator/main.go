c43202b3a76c2be3370e107c188b310a6b729e35
Kirk Kaiser <kirk@zothcorp.com>
2019-07-11 15:59:59 -0400

    Getting closer with the replacement Go service

--- golang-concurrent-generator/main.go
+++ golang-concurrent-generator/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
-	"strings"
 
 	log "github.com/Sirupsen/logrus"
 
@@ -17,7 +17,9 @@ import (
 // Define a type for a handler that provides tracing parameters as well
 type tracedHandler func(tracer.Span, *log.Entry, http.ResponseWriter, *http.Request)
 type Message struct {
-	Content string `json:"content"`
+	Concurrent int    `json:"concurrent"`
+	Total      int    `json:"total"`
+	URL        string `json:"url"`
 }
 
 func postWithContext(ctx context.Context, url, contentType string, body io.Reader) (*http.Request, error) {
@@ -29,6 +31,14 @@ func postWithContext(ctx context.Context, url, contentType string, body io.Reade
 	req.Header.Set("Content-Type", contentType)
 	if ctx != nil {
 		req = req.WithContext(ctx)
+	}
+	return req, nil
+}
+
+func getWithContext(ctx context.Context, url string) (*http.Request, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
 	}
 	return req, nil
 }
@@ -48,50 +58,91 @@ func withSpanAndLogger(t tracedHandler) func(http.ResponseWriter, *http.Request)
 	}
 }
 
-func sayHelloSuper(span tracer.Span, l *log.Entry, w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-
-	// set a tag for the current path
-	span.SetTag("url.path", message)
-
-	message = strings.TrimPrefix(message, "/")
-
-	// log with matching trace ID
-	l.WithFields(log.Fields{
-		"message": message,
-	}).Info("root url called with " + message)
-
-	js := Message{Content: message}
-	jsonValue, _ := json.Marshal(js)
-
-	req, err := postWithContext(r.Context(), "http://super-service:8081/super", "application/json", bytes.NewBuffer(jsonValue))
+func getURL(req *http.Request, c *http.Client, ch chan<- string) {
+	resp, err := c.Do(req)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	ch <- fmt.Sprintf(string(body))
+}
 
-	tr := &http.Transport{
-		MaxConnsPerHost: 10,
-	}
-
-	tracedClient := httptrace.WrapClient(&http.Client{Transport: tr})
-	resp, err := tracedClient.Do(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	rjs := Message{}
-	err = json.NewDecoder(resp.Body).Decode(&rjs)
+func getConcurrent(span tracer.Span, l *log.Entry, w http.ResponseWriter, r *http.Request) {
+	var m Message
+	err := json.NewDecoder(r.Body).Decode(&m)
 	if err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+	// set a tag for the current path
+	span.SetTag("url.request", m)
 
-	message = "Hello " + rjs.Content
+	// log with matching trace ID
+	l.WithFields(log.Fields{
+		"concurrent-requests": m.Concurrent,
+		"total-requests":      m.Total,
+		"URL":                 m.URL,
+	}).Info("root url called with %d concurrent requests", m.Concurrent)
 
+	tr := &http.Transport{
+		MaxConnsPerHost: m.Concurrent,
+	}
+
+	tracedClient := httptrace.WrapClient(&http.Client{Transport: tr})
+
+	ch := make(chan string)
+	for i := 0; i < m.Total; i++ {
+		req, err := getWithContext(r.Context(), m.URL)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		go getURL(req, tracedClient, ch)
+	}
+
+	for i := 0; i < m.Total; i++ {
+		l.WithFields(log.Fields{
+			"message": <-ch,
+		}).Info("root url called with ", m.Total)
+	}
+
+	message := fmt.Sprintf("%d concurrent requests done for a total of %d", m.Concurrent, m.Total)
+	w.Write([]byte(message))
+}
+
+func getConcurrentRandom(span tracer.Span, l *log.Entry, w http.ResponseWriter, r *http.Request) {
+	// log with matching trace ID
+	l.WithFields(log.Fields{
+		"concurrent-requests": 20,
+		"total-requests":      100,
+		"URL":                 "users",
+	}).Info("random user url called with 20 concurrent requests")
+
+	tr := &http.Transport{
+		MaxConnsPerHost: 20,
+	}
+
+	tracedClient := httptrace.WrapClient(&http.Client{Transport: tr})
+
+	ch := make(chan string)
+	for i := 0; i < 100; i++ {
+		req, err := getWithContext(r.Context(), "http://localhost")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		go getURL(req, tracedClient, ch)
+	}
+
+	for i := 0; i < 100; i++ {
+		l.WithFields(log.Fields{
+			"message": <-ch,
+		}).Info("random user url called with 20 concurrent")
+	}
+
+	message := fmt.Sprintf("20 concurrent requests done for a total of 100 random user requests")
 	w.Write([]byte(message))
 }
 
@@ -110,9 +161,9 @@ func main() {
 	defer tracer.Stop()
 
 	mux := httptrace.NewServeMux(httptrace.WithServiceName("frontend-go-service"), httptrace.WithAnalytics(true)) // init the http tracer
-	mux.HandleFunc("/generate_requests", withSpanAndLogger(sayPong))
-	mux.HandleFunc("/generate_requests_user", withSpanAndLogger(sayPong))
-	mux.HandleFunc("/", withSpanAndLogger(sayHelloSuper)) // use the tracer to handle the urls
+	mux.HandleFunc("/generate_requests", withSpanAndLogger(getConcurrent))
+	mux.HandleFunc("/generate_requests_user", withSpanAndLogger(getConcurrentRandom))
+	mux.HandleFunc("/", withSpanAndLogger(sayPong)) // use the tracer to handle the urls
 
 	err := http.ListenAndServe(":8080", mux) // set listen port
 	if err != nil {
