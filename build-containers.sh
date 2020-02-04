83b54f6d81e4b6089fae93b5d59cb8d5ceff56f3
KP Kaiser <kirk@zothcorp.com>
2020-02-04 17:32:53 -0500

    Update to latest version of containers / tracers

--- build-containers.sh
+++ build-containers.sh
@@ -1,4 +1,4 @@
-cd frontend/ && docker build -t burningion/k8s-distributed-tracingfrontend:1.1 . && cd ..
+cd frontend/ && docker build -t burningion/k8s-distributed-tracing-frontend:1.1 . && cd ..
 cd users/ && docker build -t burningion/k8s-distributed-tracing-users:1.1 . && cd ..
 cd sensors/ && docker build -t burningion/k8s-distributed-tracing-sensors:1.1 . && cd ..
 cd pumps/ && docker build -t burningion/k8s-distributed-tracing-pumps:1.1 . && cd ..
